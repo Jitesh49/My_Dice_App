b2037f19c7757c74c9741db47f7896ab4443b1a7
Jitesh49 <jitu491998@gmail.com>
2023-10-02 00:39:41 +0530

    Initial commit

---
+++ dice_1/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:basics/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
+        ),
+      ),
+    ),
+  );
+}
